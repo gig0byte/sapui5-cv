6b675827cc8d2e110d8458a0c7e4c6b8ac4081eb
pmazharov <p.mazharov@ntc-hitech.ru>
2022-08-09 23:07:49 +0300

    Removed old models

--- uimodule/webapp/controller/MasterPage.controller.js
+++ uimodule/webapp/controller/MasterPage.controller.js
@@ -5,70 +5,8 @@ sap.ui.define(
      */
     function (Controller) {
         "use strict";
-
-        let oModel = {
-            img: {
-                photo: "/static/photo.png"
-            },
-
-            contacts: [{
-                name: "email",
-                link: "mailto:gig0byte26@gmail.com",
-                value: "gig0byte26@gmail.com",
-                icon: "email"
-            },
-            {
-                name: "phone",
-                link: "tel:+79052584362",
-                value: "+7 905 258 43 62",
-                icon: "call"
-            }, {
-                name: "city",
-                value: "Saint-Petersburg, Russia",
-                icon: "map"
-            }, {
-                name: "linkedin",
-                icon: "arobase",
-                link: "https://www.linkedin.com/in/pavel-mazharov/",
-                value: "pavel-mazharov",
-            }],
-
-            assets: [{
-                name: "skills",
-                items: [{
-                    name: "SAPUI5",
-                    value: 100
-                },
-                {
-                    name: "Vue 2",
-                    value: 100
-                }, {
-                    name: "JavaScript",
-                    value: 80
-                }, {
-                    name: "Python",
-                    value: 70
-                }, {
-                    name: "Docker",
-                    value: 70
-                }]
-            },
-            {
-                name: "languages",
-                items: [{
-                    name: "Russian",
-                    value: 100
-                }, {
-                    name: "English",
-                    value: 80
-                }]
-            }]
-        };
-
         return Controller.extend("cv.cv.controller.MasterPage", {
-            onInit() {
-                this.setModel(new sap.ui.model.json.JSONModel(oModel, "detail"));
-            },
+            onInit() { },
 
             pressShowDetal() {
                 this.getModel("layout").setProperty("/layout", "TwoColumnsMidExpanded");
